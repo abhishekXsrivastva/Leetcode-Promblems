1fbee7897d45748ad1e4c7f0f9d8ba9b25fbd088
abhishekXsrivastva <abhishek18srivastva@gmail.com>
2026-07-17 20:10:20 +0545

    Time: 0 ms (100%), Space: 22.9 MB (61.63%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,27 +1,61 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        map<int, vector<int>> mpp;
-        vector<int> res;
+        int n = mat.size();
+        int m = mat[0].size();
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                mpp[i+j].push_back(mat[i][j]);
+        vector<int> res;
+        bool up = true;
+
+        int row = 0, col = 0;
+
+        for(int i = 0; i < n*m; i++){
+            res.push_back(mat[row][col]);
+
+            if(up){
+                if(col == m - 1){
+                    row++;
+                    up = false;
+                }else if(row == 0){
+                    col++;
+                    up = false;
+                }else{
+                    row--;
+                    col++;
+                }
+            }else{
+                if(row == n - 1){
+                    col++;
+                    up = true;
+                }else if(col == 0){
+                    row++;
+                    up = true;
+                }else{
+                    row++;
+                    col--;
+                }
             }
-        }
-        bool rev = true;
-        for(auto &it : mpp){
-            if(rev){
-                reverse(it.second.begin(), it.second.end());
-            }
-        
-        for(int &num : it.second){
-            res.push_back(num);
-        }
-        rev = !rev;
         }
         return res;
+        // map<int, vector<int>> mpp;
+        // vector<int> res;
+
+        // for(int i = 0; i < m; i++){
+        //     for(int j = 0; j < n; j++){
+        //         mpp[i+j].push_back(mat[i][j]);
+        //     }
+        // }
+        // bool rev = true;
+        // for(auto &it : mpp){
+        //     if(rev){
+        //         reverse(it.second.begin(), it.second.end());
+        //     }
+        
+        // for(int &num : it.second){
+        //     res.push_back(num);
+        // }
+        // rev = !rev;
+        // }
+        // return res;
     }
 };
