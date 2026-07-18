43100e640a29413f9f71e4b12a1bfb4a7a9485c9
abhishekXsrivastva <abhishek18srivastva@gmail.com>
2026-07-18 16:58:39 +0545

    Time: 2 ms (79.23%), Space: 19.3 MB (68.08%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> res;
+        vector<vector<int>> adj(numCourses);
+        for(auto &e : prerequisites){
+            adj[e[1]].push_back(e[0]);
+        }
+
+        vector<int> inDegree(numCourses);
+        for(int i = 0; i < numCourses; i++){
+            for(auto it : adj[i]){
+                inDegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            res.push_back(node);
+            for(auto it : adj[node]){
+                inDegree[it]--;
+                if(inDegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+
+        if(res.size() == numCourses) return true;
+
+        return false;
+
+    }
+};
