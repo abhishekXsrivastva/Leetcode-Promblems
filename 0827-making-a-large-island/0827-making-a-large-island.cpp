9f8a7bb5f85e673f0e906b0f17fd58586018ce83
abhishekXsrivastva <abhishek18srivastva@gmail.com>
2026-08-12 00:01:24 +0545

    Time: 168 ms (88.42%), Space: 109.7 MB (75.92%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -0,0 +1,91 @@
+class DisjoinedSet{
+public:
+   vector<int> parent;
+   vector<int> size;
+
+   DisjoinedSet(int n){
+    size.resize(n+1, 1);
+    parent.resize(n+1);
+    for(int i = 0; i <= n; i++){
+        parent[i] = i;
+    }
+   }
+
+   int findParent(int node){
+    if(parent[node] == node) return node;
+
+    return parent[node] = findParent(parent[node]);
+   }
+
+   void unionBySize(int u, int v){
+    int ulp_u = findParent(u);
+    int ulp_v = findParent(v);
+
+    if(ulp_u == ulp_v) return;
+
+    if(size[ulp_u] < size[ulp_v]){
+        parent[ulp_u] = ulp_v;
+        size[ulp_v] += size[ulp_u];
+    }else{
+        parent[ulp_v] = ulp_u;
+        size[ulp_u] += size[ulp_v];
+    }
+   }
+};
+class Solution {
+    bool isValid(int newr, int newc, int n){
+        return newr >= 0 && newr < n && newc >= 0 && newc < n;
+    }
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        DisjoinedSet dsu(n*n);
+        for(int row = 0; row < n; row++){
+            for(int col = 0; col < n; col++){
+                if(grid[row][col] == 0) continue;
+                int dr [] = {-1, 0, 1, 0};
+                int dc [] = {0, 1, 0, -1};
+                for(int i = 0; i < 4; i++){
+                    int newr = row + dr[i];
+                    int newc = col + dc[i];
+                    if(isValid(newr, newc, n) && grid[newr][newc] == 1){
+                        int nodeNo = row * n + col;
+                        int adjNodeNo = newr * n + newc;
+                        dsu.unionBySize(nodeNo, adjNodeNo);
+                    }
+                }
+            }
+        }
+        int largestIslant = 0;
+   
+        for(int row = 0; row < n; row++){
+            for(int col = 0; col < n; col++){
+                if(grid[row][col] == 1) continue;
+                set<int> parentComp;
+                int dr [] = {-1, 0, 1, 0};
+                int dc [] = {0, 1, 0, -1};
+                 for(int i = 0; i < 4; i++){
+                    int newr = row + dr[i];
+                    int newc = col + dc[i];
+                if(isValid(newr, newc, n)){
+                    if(grid[newr][newc] == 1){
+                        parentComp.insert(dsu.findParent(newr * n + newc));
+                    }
+                }
+            }
+            
+        
+        int totalSize = 0;
+        for(auto it : parentComp){
+            totalSize += dsu.size[it];
+        }
+        largestIslant = max(largestIslant, totalSize + 1);
+        }
+        }
+
+        for(int i = 0; i < n*n; i++){
+            largestIslant = max(largestIslant, dsu.size[dsu.parent[i]]);
+        }
+        return largestIslant;
+    }
+};
